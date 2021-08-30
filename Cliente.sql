14757dd1ba02ca31f53fafffc50869c89de3fce9
berna <bernajr1010@gmail.com>
2021-08-30 14:09:56 +0200

    Final

--- Cliente.sql
+++ Cliente.sql
@@ -1,7 +1,9 @@
-create table clientes(
+   create table clientes(
       id int primary key auto_increment,
       nombre varchar(50),
-      apellidos varchar(60),
+      apellido1 varchar(50),
+      apellido2 varchar(50),
+      sexo varchar(1),
       edad int,
       direccion varchar(60)
    );

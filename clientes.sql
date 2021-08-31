4ebdc449313359bcc07e07e417eea81d017f31d9
eoi.it <pedro-ji-12@hotmail.com>
2021-08-31 08:49:49 +0200

    hhh

--- clientes.sql
+++ clientes.sql
@@ -1,9 +1,7 @@
 create table clientes(
                          id int primary key auto_increment,
                          nombre varchar(50),
-                         apellido1 varchar(50),
-                         apellido2 varchar(50),
-                         sexo varchar(1),
+                         apellidos varchar(50),
                          edad int,
                          direccion varchar(50)
 );
